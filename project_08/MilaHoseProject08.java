224f326890e7a1aab3ccaef1c94c7f73debf8122
Mila Hose <mila.hose@gmail.com>
2020-03-27 10:11:28 -0700

    Add project 8

---
+++ project_08/MilaHoseProject08.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MilaHoseProject08 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner (System.in);
+    int choice = 'Y';
+
+    do {
+      System.out.print("\nEnter a number between 0 and 511: ");
+      int number = input.nextInt();
+      int[] nums = decimalToBinary(number);
+
+      System.out.println();
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+          System.out.print("H ");
+        } else {
+          System.out.print("T ");
+        }
+
+        if ((i+1) % 3 == 0) {
+          System.out.println();
+        }
+      }
+
+
+      System.out.print("\n\nWould you like to test another number? Y/N: ");
+      choice = Character.toUpperCase(input.next().charAt(0));
+      input.nextLine();
+
+    } while (choice == 'Y');
+  }
+
+  public static int[] decimalToBinary(int n) {
+    int[] nums = new int[9];
+  
+    for (int i = nums.length - 1; i >= 0 && n > 0; i--) {
+      if (n % 2 == 0) {
+        nums[i] = 0;
+      } else {
+        nums[i] = 1;
+      }
+      n /= 2;
+    }
+    
+    return nums;
+  }
+}
