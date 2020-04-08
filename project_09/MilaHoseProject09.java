d56bdd084596b7ec06f9dbde8a5f37429a5fbf52
Mila Hose <mila.hose@gmail.com>
2020-04-08 02:19:16 -0700

    Add comments

--- project_09/MilaHoseProject09.java
+++ project_09/MilaHoseProject09.java
@@ -8,23 +8,31 @@ class MilaHoseProject09 {
     do {
       Temperature[] temps = new Temperature[4];
       
+      // Initialize first element with default constructor
       temps[0] = new Temperature();
       displayTemp(temps[0], 1, 0.0, 'C');
 
+      // Get degree for 2nd Temperature object
       System.out.print("\n\nPlease choose a degree for Temperature two: ");
       double deg2 = input.nextDouble();
+      // Initialize 2nd element with degree from user
       temps[1] = new Temperature(deg2);
       displayTemp(temps[1], 2, deg2, 'C');
 
+      // Get degree for 3rd Temperature object
       System.out.print("\n\nPlease choose a scale for Temperature three: ");
       char scale3 = Character.toUpperCase(input.next().charAt(0));
+      // Initialize 3rd element with scale from user
       temps[2] = new Temperature(scale3);
       displayTemp(temps[2], 3, 0.0, scale3);
 
+      // Get degree for 4th Temperature object
       System.out.print("\n\nPlease choose a degree for Temperature four: ");
       double deg4 = input.nextDouble();
+      // Get scale for 4th Temperature object
       System.out.print("Please choose a scale for Temperature four: ");
       char scale4 = Character.toUpperCase(input.next().charAt(0));
+      // Initialize 4th element with degree and scale from user
       temps[3] = new Temperature(deg4, scale3);
       displayTemp(temps[3], 4, deg4, scale3);
 
@@ -32,6 +40,7 @@ class MilaHoseProject09 {
       displayTempEquality(temps);
       
 
+      // Ask user if they want to repeat the test
       // ========================================================
       System.out.print("\n\nWould you like to try again? Y/N: ");
       choice = Character.toUpperCase(input.next().charAt(0));
@@ -44,6 +53,7 @@ class MilaHoseProject09 {
     Scanner input = new Scanner (System.in);
     String num = "";
 
+    // Set the initialize message based on which constructor is used
     switch(n) {
       case 1: 
         num = "first";
@@ -86,6 +96,7 @@ class MilaHoseProject09 {
 
   public static void displayTempsCreated(Temperature[] temps) {
     int count = 0;
+    // Display all degrees Celcius
     System.out.print("\nIn order of creation the Temperatures in Celcius are: ");
     while (count < 4) {
       System.out.print(String.format("%.2f", temps[count].getDegreeInCelsius()) + (count == 3 ? ". " : ", "));
@@ -93,6 +104,7 @@ class MilaHoseProject09 {
     }
 
     count = 0;
+    // Display all degrees Fahrenheit and reset count
     System.out.print("\nIn order of creation the Temperatures in Fahrenheit are: ");
     while (count < 4) {
       System.out.print(String.format("%.2f", temps[count].getDegreeInFahrenheit()) + (count == 3 ? ". " : ", "));
@@ -105,6 +117,7 @@ class MilaHoseProject09 {
   public static void displayTempEquality(Temperature[] temps) {
     for (int i = 0; i < 3; i++) {
       for (int j = i; j < 3; j++) {
+        // Iterate through array of temps and display equalities for each
         System.out.println("The " + placeKey(i + 1) + " Temperature is" + 
         (temps[i].equals(temps[j + 1]) ? " " : " not ") + "equal to the " + placeKey(j + 2) + ".");
 
@@ -119,6 +132,7 @@ class MilaHoseProject09 {
     }
   }
 
+  // Get place key based on index
   public static String placeKey(int n) {
     if (n == 1) {
       return "first";
