c4062e5d73b577f377a89c8190d97b307487970d
Mila Hose <mila.hose@gmail.com>
2020-04-11 00:10:43 -0700

    Finish up project 10

--- project_10/MilaHoseProject10.java
+++ project_10/MilaHoseProject10.java
@@ -1,15 +1,121 @@
+import java.util.Scanner;
+
 class MilaHoseProject10 {
-  public static void main(String[] args) {
-    char[] someString = {'H', 'E', 'L', 'L', 'o', ' ', 'w', 'o', 'R', 'l', 'D', '!'};
-    char[] someOthStr = {'H', 'E', 'L', 'L', 'o', ' ', 'w', 'o', 'R', 'l', 'D'};
-    MyString myString = new MyString(someString);
-    MyString myOthStr = new MyString(someOthStr);
-    System.out.println(myString.charAt(6));
-    System.out.println(myString.length());
-    System.out.println(myString.substring(6, 11));
-    System.out.println(myString.toLowerCase());
-    System.out.println(myString.equals(myOthStr));
-    System.out.println(myString.valueOf(12345));
+	public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int choice = 0;
+
+    // Get a test string from user
+    System.out.print("\nPlease enter a string to instantiate: ");
+    String str = input.nextLine();
+
+    char[] arr = new char[str.length()];
+
+    // Break down string into array of chars
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = str.charAt(i);
+    }
+
+    // Create a MyString test object
+    MyString testStr = new MyString(arr);
+
+    do {
+      printMenu();
+      System.out.print("\nPlease choose from the menu options above: ");
+      choice = input.nextInt();
+
+      switch (choice) {
+        case 1:
+          // .charAt() test
+          int idx;
+          do {
+            System.out.print("\nPlease provide an index value between 0-" + (testStr.length() - 1) + ": ");
+            idx = input.nextInt();
+          } while (idx < 0 || idx >= testStr.length());
+
+          System.out.println("\nThe character at index " + idx + " is '" + testStr.charAt(idx) + "'.");
+          break;
+        case 2:
+          // .length() test
+          System.out.println("\nThe length of the string '" + str + "' is " + testStr.length() + ".");
+          break;
+        case 3:
+          // .substring() test
+          int startIdx;
+          int endIdx;
+
+          do {
+            // Get start index for substring
+            System.out.print("\nPlease provide a start index value between 0-" + (testStr.length() - 1) + ": ");
+            startIdx = input.nextInt();
+          } while (startIdx < 0 || startIdx >= testStr.length());
+
+          do {
+            // Get end index for substring
+            System.out.print("\nPlease provide an end index value between 0-" + (testStr.length() - 1) + ": ");
+            endIdx = input.nextInt();
+          } while (endIdx < 0 || endIdx >= testStr.length());
+
+          MyString substr = testStr.substring(startIdx, endIdx);
+          System.out.print("\nThe value of the substring is: ");
+          substr.getString();
+          System.out.println(".");
+          break;
+        case 4:
+          // .toLowerCase() test
+          System.out.println("\nThe lowercase version of the string is: '" + testStr.toLowerCase() + ".");
+          break;
+        case 5:
+          // .equals() test
+          System.out.print("\nPlease enter another string to test against: ");
+          input.nextLine();
+          String str2 = input.nextLine();
+
+          char[] arr2 = new char[str2.length()];
+
+          // Break down test string into array of chars
+          for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = str2.charAt(i);
+          }
+
+          // Create a new MyString test object
+          MyString testStr2 = new MyString(arr2);
+
+          System.out.println("");
+          System.out.print("'");
+          testStr.getString();
+          System.out.print("' is" + (testStr.equals(testStr2) ? "" : " not") + " equal to '");
+          testStr2.getString();
+          System.out.println("'.");
+          break;
+        case 6:
+          System.out.print("\nPlease enter an integer to test: ");
+          int i = input.nextInt();
+
+          MyString valueOfTest = testStr.valueOf(i);
+          System.out.print("\nThe integer value of " + i + " is: ");
+          valueOfTest.getString();
+          break;
+      }
+    } while (choice != 7);
+
+    // End program
+    System.out.println("\nThanks for testing. Goodbye!\n");
+    System.exit(0);
+  }
+
+  // display menu
+  public static void printMenu() {
+    System.out.println("\n========================================");
+    System.out.println("                  MENU                  ");
+    System.out.println("1. Test the .charAt() method            ");
+    System.out.println("2. Test the .length() method            ");
+    System.out.println("3. Test the .substring() method         ");
+    System.out.println("4. Test the .toLowerCase() method       ");
+    System.out.println("5. Test the .equals() method            ");
+    System.out.println("6. Test the .valueOf() method           ");
+    System.out.println("7. Quit                                 ");
+    System.out.println("========================================\n");
   }
 }
 
@@ -92,5 +198,11 @@ class MyString {
     }
 
     return (new MyString(value));
+  }
+
+  public void getString() {
+    for (int i = 0; i < data.length; i++) {
+      System.out.print(data[i]);
+    }
   }
 }
